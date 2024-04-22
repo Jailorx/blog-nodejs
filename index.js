ec8dc2389f143696aae23f801744636eb7d27b63
Shubham Bisht <bisht.shubham.emp@gmail.com>
2024-04-22 22:57:44 +0530

    Add ejs template boiler plate

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 import dotenv from "dotenv";
 dotenv.config();
-
 import express from "express";
+import expressEjsLayouts from "express-ejs-layouts";
 
 const app = express();
 const PORT = process.env.PORT || 9000;
+
+//Templating Engine
+app.use(expressEjsLayouts);
+app.set("layout", "./layouts/main");
+app.set("view engine", "ejs");
 
 app.get("", (req, res) => {
   res.send("Welcome to my blog application");
