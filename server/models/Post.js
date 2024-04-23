b91f17426f1856d1b8c04caa5e81970111df5869
Shubham Bisht <bisht.shubham.emp@gmail.com>
2024-04-23 15:57:32 +0530

    Create Post model

---
+++ server/models/Post.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const postSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Post = mongoose.model("Post", postSchema);
