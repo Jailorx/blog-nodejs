10c16874ea5147502eeaa4a69f9acf0f14609d37
Shubham Bisht <bisht.shubham.emp@gmail.com>
2024-04-23 10:27:33 +0530

    Add ejs views  in routes

--- server/routes/main.js
+++ server/routes/main.js
@@ -3,7 +3,7 @@ import express from "express";
 const router = express.Router();
 
 router.get("", (req, res) => {
-  res.send("Welcome to my blog application");
+  res.render("index");
 });
 
 export default router;
