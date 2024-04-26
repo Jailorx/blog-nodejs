c6a9ade92a4c0a8160fb74bda99fc4eb0f150dad
Shubham Bisht <bisht.shubham.emp@gmail.com>
2024-04-26 12:23:39 +0530

    Implement logic to show search bar

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,11 @@
-console.log("posts:", posts);
+document.addEventListener("DOMContentLoaded", () => {
+  const searchBtn = document.querySelector(".searchBtn");
+  const searchBar = document.querySelector(".searchBar");
+  const searchInput = document.getElementById("searchInput");
+
+  searchBtn.addEventListener("click", function () {
+    searchBar.style.display = "block";
+    this.setAttribute("aria-expanded", "true");
+    searchInput.focus();
+  });
+});
