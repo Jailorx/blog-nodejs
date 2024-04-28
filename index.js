8e7b6b745a3d97ce2773ddd46b62abd3ea8637cd
Shubham Bisht <bisht.shubham.emp@gmail.com>
2024-04-28 11:29:06 +0530

    Add register page and add middlewares

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 import expressEjsLayouts from "express-ejs-layouts";
+import cookieParser from "cookie-parser";
+import MongoStore from "connect-mongo";
+import session from "express-session";
+
 import mainRoutes from "./server/routes/main.js";
 import adminRoutes from "./server/routes/admin.js";
 import connectDB from "./server/config/db.js";
@@ -14,6 +18,17 @@ connectDB();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cookieParser());
+app.use(
+  session({
+    secret: "alchemist",
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGODB_URI,
+    }),
+  })
+);
 
 app.use(express.static("public"));
 
--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,4 +1,8 @@
 import express from "express";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { Post } from "../models/Post.js";
+import { User } from "../models/User.js";
 
 const router = express.Router();
 
@@ -6,7 +10,29 @@ const adminLayout = `../views/layouts/admin`;
 
 router.get("/admin", async (req, res) => {
   try {
-    res.render("admin/index", { layout: adminLayout });
+    res.render("admin/signin", { layout: adminLayout });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.get("/admin/register", async (req, res) => {
+  try {
+    res.render("admin/register", { layout: adminLayout });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.post("/admin", async (req, res) => {
+  try {
+    // console.log(req.body);
+    const { username, password } = req.body;
+    if (username === "admin" && password === "password")
+      res.send("you are logged in");
+    else {
+      res.send("wrong password or username");
+    }
   } catch (error) {
     console.log(error);
   }
